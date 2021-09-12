5380caabea66dd4e30083153987a4e5a0939312a
Kirill Akulich <kirillakulich0@gmail.com>
2021-09-12 08:16:35 +0300

    init

---
+++ lab1/method_middle_square.py
@@ -0,0 +1,21 @@
+import time
+
+
+def get_middle_interval(number:int,interal_length:int)->int:
+    assert(interal_length%2 is 0)
+    number_str:str = str(number)
+
+
+def middle_square_generator(seed:int)->int:
+    interval_length:int = len(str(seed))
+    previous_value= seed
+    while True:
+        next_value = 
+
+if __name__=="__main__":
+    seed_str = str(time.time()).replace('.','')[-8:]
+    seed= int(seed_str)
+    print(seed)
+
+
+
