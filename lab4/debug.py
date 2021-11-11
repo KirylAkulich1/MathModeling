14d1456331e47353d9ed15bb7cde62c2b688ca22
Kirill Akulich <kirill.akulich@softeq.com>
2021-11-11 07:46:50 +0300

    debig added

---
+++ lab4/debug.py
@@ -0,0 +1,14 @@
+import networkx as nx
+import numpy as np 
+# importing matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+def do_iterate(D_m,D,m_0):
+    options_to_check = list()
+    state_tree = dict()
+    m = m_0
+    
+D_m=[]
+D_p=[]
+D
+mu_0 = []
